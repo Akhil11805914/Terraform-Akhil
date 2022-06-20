ac773fdad07bb8bc0a76db0264ea8cac5ac57c15
Sai Akhil Reddy Ankireddy <63990882+Akhil11805914@users.noreply.github.com>
2022-06-20 10:53:33 +0530

    Update Variables.tf

--- Variables.tf
+++ Variables.tf
@@ -13,16 +13,3 @@ variable "key_pair" {
     description = "Key Pair you want to attach to the instance"
     default="Terraform-Test"
 }
-variable "instance_type_list" {
-    type = list(string)
-    description = "This is an list of instance types we can use fo EC2"
-    default=["t2.micro","t3.micro"]
-}
-variable "instance_type_map" {
-    type = map(string)
-    description = "Here are types of instances you can use in different types of environments"
-    default = {
-        "prod" = "t3.micro"
-        "dev"  = "t2.micro"
-    }
-}
